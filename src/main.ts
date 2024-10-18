39582271a375767537d68f0df9875fc5b04b7abf
Rishat <virisound@gmail.com>
2024-10-19 02:13:35 +0500

    Deploy: setting main.ts and .env

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api') // станет: http://localhost:3000/api
-  app.enableCors(); // для предоставления доверия обмена данными между источниками (доменами и поддоменами) запроса - CORS (безопасность между источниками): https://docs.nestjs.com/security/cors#getting-started
-  await app.listen(3000);
+  app.setGlobalPrefix('api') // станет: http://localhost:3001/api
+  app.enableCors({ // для предоставления доверия обмена данными между источниками (доменами и поддоменами) запроса - CORS (безопасность между источниками): https://docs.nestjs.com/security/cors#getting-started
+    origin: ['http://localhost:3000', 'http://45.137.153.37:3000'], //! public ip VPS
+  }); 
+  await app.listen(3001); //! какой порт слушать (порт бэкенда сервера)(на каком сидит бэкенд)
 }
 bootstrap();
